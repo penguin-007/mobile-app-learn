4ff3632401f8f68bf79c4aa4f6c539da11d807c3
mr.laznevoy <mr.laznevoy@gmail.com>
2018-11-10 16:45:28 +0200

    Сделал страницу логина

--- Panda/src/app/user/login/login.component.ts
+++ Panda/src/app/user/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Page } from "tns-core-modules/ui/page";
 import { Router } from "@angular/router";
 
+import * as appSettings from "tns-core-modules/application-settings";
+
 @Component({
   selector: 'ns-login',
   templateUrl: './login.component.html',
@@ -10,8 +12,8 @@ import { Router } from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
-  email = '1';
-  password = '2';
+  email = '';
+  password = '';
 
   constructor(
     private page: Page,
@@ -21,10 +23,18 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.page.actionBarHidden = true;
+
+    // settings
+    this.email = appSettings.getString("email");
+    if (this.email !== undefined) {
+      this.router.navigate(["/home"]);
+    }
+    console.log('this.email', this.email);
   }
 
   submit() {
     console.log('btn login click', this.email, this.password);
+    appSettings.setString("email", this.email);
     this.router.navigate(["/home"]);
   }
 
