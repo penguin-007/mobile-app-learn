6e38604a266c896f1d43ddaad38e8506cab2eb92
Alexander <mr.laznevoy@gmail.com>
2018-11-11 16:08:49 +0200

    2

--- Panda/src/app/projects/projects-routing.module.ts
+++ Panda/src/app/projects/projects-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { ProjectsListComponent } from "./projects-list/projects-list.component";
+import { ReportsListComponent } from "../reports/reports-list/reports-list.component";
 
 const routes: Routes = [
-    { path: "", component: ProjectsListComponent }
+    { path: "", component: ProjectsListComponent },
+    { path: "project/:id", component: ReportsListComponent }
 ];
 
 @NgModule({
--- Panda/src/app/projects/projects.module.ts
+++ Panda/src/app/projects/projects.module.ts
@@ -3,13 +3,17 @@ import { NativeScriptCommonModule } from 'nativescript-angular/common';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 
 import { ProjectsRoutingModule } from "./projects-routing.module";
+import { ReportsListComponent } from '../reports/reports-list/reports-list.component';
 
 @NgModule({
   imports: [
     NativeScriptCommonModule,
-    ProjectsRoutingModule
+    ProjectsRoutingModule,
   ],
-  declarations: [ProjectsListComponent],
+  declarations: [
+    ProjectsListComponent,
+    ReportsListComponent
+  ],
   schemas: [NO_ERRORS_SCHEMA]
 })
 export class ProjectsModule { }
