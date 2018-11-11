4d7155e30e27d18fc3dc9ec8fb2ba5d616195f30
Alexander <mr.laznevoy@gmail.com>
2018-11-11 16:54:54 +0200

    Добавил вывод проектов

--- Panda/src/app/reports/reports-list/reports-list.component.ts
+++ Panda/src/app/reports/reports-list/reports-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PageRoute } from "nativescript-angular/router";
 import { switchMap } from "rxjs/operators";
+import { ReportsService } from '~/app/shared/reports/reports.service';
 
 @Component({
   selector: 'ns-reports-list',
@@ -10,10 +11,19 @@ import { switchMap } from "rxjs/operators";
 })
 export class ReportsListComponent implements OnInit {
 
+  protected token = '5602021ae760ac1f3b3307f74f5ff522';
+
+  reportsArray = [];
+  // todo добавить модель проекта
+
+  isLoading = false;
+  listLoaded = false;
+
   projectId: number;
 
   constructor(
-    private pageRoute: PageRoute
+    private pageRoute: PageRoute,
+    private reportsService: ReportsService
   ) {
     // use switchMap to get the latest activatedRoute instance
     this.pageRoute.activatedRoute.pipe(
@@ -22,11 +32,23 @@ export class ReportsListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getReports(this.projectId);
+    this.getReports(this.token, this.projectId);
   }
 
-  getReports(id) {
-    console.log('id', this.projectId);
+  getReports(token, projectId) {
+    this.reportsService.getReports(token, projectId).subscribe(result => {
+      if (result['body'].results !== undefined) {
+        // console.log("getProjects", result['body'].results);
+        this.reportsArray = result['body'].results;
+        // console.log('this.reportsArray', this.reportsArray);
+        setTimeout(() => {
+          this.isLoading = false;
+          this.listLoaded = true;
+        }, 500);
+      }
+    }, error => {
+        console.error("getProjects", error);
+    });
   }
 
 }
