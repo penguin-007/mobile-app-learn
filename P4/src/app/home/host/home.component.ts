a9a0eb1b2b05fc21759ede2ac2469835a83deb1d
Alexander <mr.laznevoy@gmail.com>
2018-11-15 22:46:14 +0200

    Добавил вывод данных пользователя на главный экран

--- P4/src/app/home/host/home.component.ts
+++ P4/src/app/home/host/home.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 
+import * as appSettings from "tns-core-modules/application-settings";
+import { UserService } from "~/app/shared/user/user.service";
+
 @Component({
     selector: "Home",
     moduleId: module.id,
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
 
+    user;
+
     constructor(
+        private userService: UserService
     ) {
         // Use the component constructor to inject providers.
     }
@@ -17,10 +24,30 @@ export class HomeComponent implements OnInit {
     ngOnInit(): void {
         // Init your component properties here.
         console.log('home ngOnInit');
+
+        // get token
+        let token = appSettings.getString("token");
+        this.getUserData(token);
+        console.log('app comp on init', token);
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    // get user data by token
+    getUserData(token) {
+        this.userService.userGetData(token).subscribe(result => {
+            if (result.body !== undefined) {
+                // console.log("getUserData", result['body']);
+                this.user = result.body.results;
+            } else {
+                console.warn("getUserData", 'noData');
+            }
+        }, error => {
+            console.error("getUserData", error);
+            alert('Ошибка токена');
+        });
     }
 }
