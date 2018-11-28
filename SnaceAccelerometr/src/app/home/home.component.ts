cc9b24067f1f79405dcf08d13533a70e6502cca8
Alexander <mr.laznevoy@gmail.com>
2018-11-28 22:00:14 +0200

    wdwadad

--- SnaceAccelerometr/src/app/home/home.component.ts
+++ SnaceAccelerometr/src/app/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 
+// var accelerometer = require("nativescript-accelerometer");
+
+// import * as accelerometer from "nativescript-accelerometer";
+import * as accelerometer from "nativescript-accelerometer-advanced";
+
+
 @Component({
     selector: "Home",
     moduleId: module.id,
@@ -8,10 +14,22 @@ import { Component, OnInit } from "@angular/core";
 export class HomeComponent implements OnInit {
 
     constructor() {
+        console.log('ngOnInit');
+
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+        console.log('ngOnInit');
+
+        // accelerometer.startAccelerometerUpdates(function(data) {
+        //     console.log("x: " + data.x + "y: " + data.y + "z: " + data.z);
+        // }, { sensorDelay: "ui" });
+
+        accelerometer.startAccelerometerUpdates(function(data) {
+            console.log(" X: " + data.x + " Y: " + data.y + " Z: " + data.z + " Sensor Type: " + data.sensortype + " Time in milliseconds : " + data.timemilli);
+        }, { sensorDelay: "ui" });
+
     }
 }
