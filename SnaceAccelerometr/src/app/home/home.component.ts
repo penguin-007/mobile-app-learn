11c781d38b84ef5544e10f121783bcdef6747032
Alexander <mr.laznevoy@gmail.com>
2018-11-29 22:09:24 +0200

    Добавил катающийся шарик на AbsoluteLayout

--- SnaceAccelerometr/src/app/home/home.component.ts
+++ SnaceAccelerometr/src/app/home/home.component.ts
@@ -8,12 +8,15 @@ import * as accelerometer from "nativescript-accelerometer";
 @Component({
     selector: "Home",
     moduleId: module.id,
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-    xCord = 0;
-    yCord;
+    xCord = 135;
+    yCord = 135;
+
+    speed = 50;
 
     constructor() {
         console.log('ngOnInit');
@@ -29,8 +32,29 @@ export class HomeComponent implements OnInit {
     start() {
         this.xCord++;
         accelerometer.startAccelerometerUpdates((data) => {
-            this.xCord = data.x;
-            console.log('this.xCord', this.xCord);
+            // x
+            const xTemp = this.xCord + data.x * this.speed;
+
+            if (xTemp >= 270) {
+                this.xCord = 270;
+            } else if (xTemp <= 0) {
+                this.xCord = 0;
+            } else {
+                this.xCord = xTemp;
+            }
+
+            // y
+            const yTemp = this.yCord + -data.y * this.speed;
+
+            if (yTemp >= 270) {
+                this.yCord = 270;
+            } else if (yTemp <= 0) {
+                this.yCord = 0;
+            } else {
+                this.yCord = yTemp;
+            }
+
+            // console.log('this.xCord', xTemp);
             // console.log("x: " + data.x + "y: " + data.y + "z: " + data.z);
         }, { sensorDelay: "ui" });
     }
