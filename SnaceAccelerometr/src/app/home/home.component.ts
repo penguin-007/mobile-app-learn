990a959d19df95296d5d2f4b5ecf65de019a3c68
Alexander <mr.laznevoy@gmail.com>
2018-11-29 22:38:20 +0200

    переделал на перемещение по FPS

--- SnaceAccelerometr/src/app/home/home.component.ts
+++ SnaceAccelerometr/src/app/home/home.component.ts
@@ -16,46 +16,66 @@ export class HomeComponent implements OnInit {
     xCord = 135;
     yCord = 135;
 
-    speed = 50;
+    foodYCord = 50;
+    foodXCord = 50;
+
+    step = 20;
+
+    fps = 1000 / 60;
+    deltaX: number;
+    deltaY: number;
+
+    intervalId;
 
     constructor() {
         console.log('ngOnInit');
         // Use the component constructor to inject providers.
     }
     
-
     ngOnInit(): void {
         // Init your component properties here.
         console.log('ngOnInit');
     }
 
     start() {
-        this.xCord++;
+        this.startAccelerometr();
+        this.startGame();
+    }
+
+    startAccelerometr() {
         accelerometer.startAccelerometerUpdates((data) => {
-            // x
-            const xTemp = this.xCord + data.x * this.speed;
-
-            if (xTemp >= 270) {
-                this.xCord = 270;
-            } else if (xTemp <= 0) {
-                this.xCord = 0;
-            } else {
-                this.xCord = xTemp;
-            }
-
-            // y
-            const yTemp = this.yCord + -data.y * this.speed;
-
-            if (yTemp >= 270) {
-                this.yCord = 270;
-            } else if (yTemp <= 0) {
-                this.yCord = 0;
-            } else {
-                this.yCord = yTemp;
-            }
-
-            // console.log('this.xCord', xTemp);
-            // console.log("x: " + data.x + "y: " + data.y + "z: " + data.z);
+            this.deltaX = data.x;
+            this.deltaY = data.y;
         }, { sensorDelay: "ui" });
+    }
+
+    startGame() {
+        this.intervalId = setInterval(() => {
+            this.moveSnake(this.deltaX, this.deltaY);
+        }, this.fps);
+    }
+
+    getBoundariesFood(x, y) {
+    }
+
+    moveSnake(deltaX, deltaY) {
+        const xTemp = this.xCord + this.step * deltaX;
+        const yTemp = this.yCord + this.step * -deltaY;
+
+        if (xTemp >= 270) {
+            this.xCord = 270;
+        } else if (xTemp <= 0) {
+            this.xCord = 0;
+        } else {
+            this.xCord = xTemp;
+        }
+        
+        if (yTemp >= 270) {
+            this.yCord = 270;
+        } else if (yTemp <= 0) {
+            this.yCord = 0;
+        } else {
+            this.yCord = yTemp;
+        }
     }
 }
