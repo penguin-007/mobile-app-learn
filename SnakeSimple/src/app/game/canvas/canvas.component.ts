da4497fe6d061dba7814546d0f795ebe45035863
Alexander <mr.laznevoy@gmail.com>
2018-12-02 12:42:12 +0200

    Добавил Head Snake

--- SnakeSimple/src/app/game/canvas/canvas.component.ts
+++ SnakeSimple/src/app/game/canvas/canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Snake } from '~/app/models/snake.model';
 
 import { removeCallback, start, stop, addCallback } from "tns-core-modules/fps-meter";
@@ -9,11 +9,47 @@ import { removeCallback, start, stop, addCallback } from "tns-core-modules/fps-m
   styleUrls: ['./canvas.component.css'],
   moduleId: module.id,
 })
-export class CanvasComponent implements OnInit {
+export class CanvasComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  snake: Snake;
+
+  fps = 1000 / 1; // frame per second
+  intervalId;
+
+  gameRuned: Boolean;
+
+  constructor() {
+    this.gameRuned = false;
+  }
 
   ngOnInit() {
+    this.snake = new Snake;
+  }
+
+  ngOnDestroy() {
+    this.canvasEnd();
+  }
+
+  startGame() {
+    if (!this.gameRuned) {
+      this.canvasStart();
+      this.gameRuned = true;
+    }
+  }
+
+  endGame() {
+    this.canvasEnd();
+    this.gameRuned = false;
+  }
+
+  canvasStart() {
+    this.intervalId = setInterval(() => {
+      this.snake.move();
+    }, this.fps);
+  }
+
+  canvasEnd() {
+    clearInterval(this.intervalId);
   }
 
 }
--- SnakeSimple/src/app/models/snake.model.ts
+++ SnakeSimple/src/app/models/snake.model.ts
@@ -3,24 +3,55 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Snake {
 
-    radius: Number;
-    length: Number;
+    radius: number;
+    length: number;
+
+    headX;
+    headY;
+
+    startPositionX: number;
+    startPositionY: number;
+
+    direction: String;
+    step: number;
 
     constructor() {
+        this.length = 0;
+        this.radius = 10;
+
+        this.setStartPosition(150, 150);
+
+        this.headX = this.startPositionX;
+        this.headY = this.startPositionY;
+
+        this.direction = 'left';
+        this.step = this.radius * 2;
     }
 
     // 
-    moveStart(): void {
-
+    move(): void {
+        switch (this.direction) {
+            case 'left':
+                this.headX += this.step;
+                break;
+            default:
+                break;
+        }
     }
 
     // 
     moveEnd(): void {
-
+        this.headX = this.startPositionX;
+        this.headY = this.startPositionY;
     }
 
     moveDirection(direstion: String): void {
 
+    }
+
+    setStartPosition(x, y) {
+        this.startPositionX = x - this.radius;
+        this.startPositionY = y - this.radius;
     }
 
 
