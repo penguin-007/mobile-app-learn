69897ae4a1ec8c610726528dbc82e373f7659e8b
Alexander <mr.laznevoy@gmail.com>
2018-12-13 21:32:25 +0200

    SimpleSnake: отделил модель tail

--- SnakeSimple/src/app/models/snake.model.ts
+++ SnakeSimple/src/app/models/snake.model.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Tail }  from './tail.model';
 
 @Injectable()
 export class Snake {
@@ -114,57 +115,3 @@ export class Snake {
 
 }
 
-
-export class Tail {
-
-    length;
-    radius;
-
-    tail = [];
-
-    constructor(length, radius) {
-        this.length = length;
-        this.radius = radius;
-    }
-
-    renderTall(headX, headY) {
-        for (let i = 0; i < this.length; i++) {
-            if (i === 0) {
-                this.tail.push({
-                    i: i,
-                    x: headX - this.radius * 2,
-                    y: headY
-                });
-            } else {
-                this.tail.push({
-                    i: i,
-                    x: this.tail[i-1].x - this.radius * 2,
-                    y: headY
-                });
-            }
-        }
-        return this.tail;
-    }
-
-    replaceTail(headX, headY) {
-        for (let i = this.tail.length - 1; i >= 0; i--) {
-            if (i === 0) {
-                this.tail[i].x = headX;
-                this.tail[i].y = headY;
-            } else {
-                this.tail[i].x = this.tail[i-1].x;
-                this.tail[i].y = this.tail[i-1].y;
-            }
-        }
-        return this.tail;
-    }
-
-    addPart() {
-        this.tail.push({
-            i: this.tail.length + 1,
-            x: this.tail[this.tail.length - 1].x,
-            y: this.tail[this.tail.length - 1].y
-        });
-    }
-
-}
